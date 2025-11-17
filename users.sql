8cbfdebe1cb850183a09d3766feb011558daff69
trentkingston2007-del <trentkingston2007@gmail.com>
2025-11-17 18:09:00 -0500

    Update users.sql

--- users.sql
+++ users.sql
@@ -4,6 +4,6 @@ CREATE TABLE users (
   password VARCHAR(255) NOT NULL,
   email VARCHAR(120) NOT NULL UNIQUE,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `rank` ENUM('user','moderator','admin','owner') DEFAULT 'user',
+  role ENUM('user','moderator','admin','owner') DEFAULT 'user',
   status ENUM('active','banned','suspended','pending') DEFAULT 'pending'
 );
