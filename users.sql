3b1481c76b800a026746589d5f3052b23f2f656d
trentkingston2007-del <trentkingston2007@gmail.com>
2025-11-17 18:08:33 -0500

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(120) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    rank ENUM('user','moderator','admin','owner') DEFAULT 'user',
-    status ENUM('active','banned','suspended','pending') DEFAULT 'pending'
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(120) NOT NULL UNIQUE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `rank` ENUM('user','moderator','admin','owner') DEFAULT 'user',
+  status ENUM('active','banned','suspended','pending') DEFAULT 'pending'
 );
